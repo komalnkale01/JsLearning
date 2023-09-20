65412157a51528f9c68b5318f8bc6e72619e30e1
komalnkale01 <komalnkale01@gmail.com>
2023-09-20 19:24:32 +0530

    Variable Assinment No.1 Added
    Variable Assinment No.1 Added

---
+++ 01_variableAssign.js
@@ -0,0 +1,44 @@
+
+var myFullName; // variable declaration
+console.log( myFullName); //variable initialization
+
+ myFullName ="komal  kale"; //variable initialization
+console.log(myFullName);
+myFullName = "mss.komal nilesh kale" ;//variable update
+console.log( myFullName);
+
+
+var myLove = " I love only JS" ; // variable declaration and initialization
+console.log( myLove);
+
+
+var firstName = "komal";  // variable declaration and initialization
+var lastName = " kale";  // variable declaration and initialization
+
+console.log(" First Name :",firstName);
+console.log( "Last Name :", lastName);
+
+var age = " 28" // variable declaration 
+console.log( "age :", age); // initialization
+
+var address = " pune "; // variable declaration and initialization
+console.log("Address :", address);
+
+var pincode = " 412303 ";  // variable declaration 
+console.log ("pincode :",pincode);// initialization
+
+var gender = " Female" // variable declaration and initialization
+console.log("gender :", gender);
+
+var isMarried = " yes i am "
+console.log( "is married:", isMarried);
+ 
+
+ 
+
+
+ 
+
+ 
+
+
