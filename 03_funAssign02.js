adbc53ea55fc2e2aea1cc87e2cea070bca1d1350
komalnkale01 <komalnkale01@gmail.com>
2023-09-25 21:35:19 +0530

    stringbasics added
    stringbasics added

---
+++ 03_funAssign02.js
@@ -0,0 +1,9 @@
+
+function bankDetails(bankName, accountNum, location, pinCode){
+
+    console.log("Bank Name-:", bankName,"Account No:", accountNum, "Location", location,"Pincode:",pinCode );
+}
+
+bankDetails("CITI Bank","3456782345","Pune",431202);
+bankDetails("Axis Bank","335352345","Mumbai",441202);
+bankDetails("HDFC Bank","35346346345","Pune",4645646,"Open");
---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,11 @@
+
+function stringBasic(){
+    console.log(" My Dream Compnay name is: ", "Google" );
+    var hobby1 = "Coding";
+    var hobby2 = "Travelling";
+    var hobby3 = "Cooking";
+    console.log(hobby1,hobby2,hobby3);
+    console.log("Sum of Total hobbies Characters: ",hobby1.length+hobby2.length+hobby3.length);
+}
+stringBasic();
+
